8660da478d77316d29e5bca5314b3f57f9e7a609
Scott <scott@concar.co.nz>
2015-09-14 13:42:03 +1200

    Lesson 5 Complete

---
+++ app/src/main/java/com/example/android/sunshine/app/data/MyView.java
@@ -0,0 +1,31 @@
+package com.example.android.sunshine.app.data;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MyView extends View
+{
+    public MyView(Context context)
+    {
+        super(context);
+    }
+    public MyView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+    public MyView(Context context, AttributeSet attrs, int defaultStyle)
+    {
+        super(context, attrs, defaultStyle);
+    }
+    @Override
+    protected void onMeasure(int wMeasureSpec, int hMeasureSpec)
+    {
+    }
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+
+    }
+}
